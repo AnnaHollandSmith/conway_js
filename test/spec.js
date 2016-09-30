0ec80560d51ad0b242aa1359581e03cd438cd91c
Anna <anna.hollandsmith3@gmail.com>
2016-09-30 17:10:03 +0100

    refactor of tests

--- test/spec.js
+++ test/spec.js
@@ -3,19 +3,22 @@ describe('Conways game of life', function() {
     expect(true).toBe(true);
   });
 
+  var board, cell;
+
+  beforeEach(function() {
+    board = new Board();
+    cell = new Cell(1,1,true);
+    board.addCell(cell);
+  });
+
   describe('addCell', function() {
     it('adds a cell to the board', function() {
-      var board = new Board();
-      var cell = new Cell(1,1,true);
-      board.addCell(cell);
       expect(board.cells.x1y1).toEqual(cell);
     });
   });
+
   describe('getCellAt', function() {
     it("returns the cell for the given coordinates", function() {
-      var board = new Board();
-      var cell = new Cell(1,1,true);
-      board.addCell(cell);
       expect(board.getCellAt(1,1)).toEqual(cell);
     });
   });
