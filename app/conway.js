004eb32ddcd59d15d8f2095e42941537ce0a9e86
Anna <anna.hollandsmith3@gmail.com>
2016-10-03 23:46:32 +0100

    expanded getAliveNeighbours method

--- app/conway.js
+++ app/conway.js
@@ -1,24 +1,57 @@
-var Board = function() {
+var Board = function () {
   this.cells = {};
 };
 
-var Cell = function(x,y) {
-  this.x = x;
-  this.y = y;
+Board.prototype = {
+  addCell: function(cell){
+    this.cells[getCellRepresentation(cell.x, cell.y)] = cell;
+  },
+  getCellAt: function(x, y){
+      return this.cells[getCellRepresentation(x, y)];
+    },
+  getAliveNeighbours: function(cell){
+    var x = cell.x;
+     var y = cell.y;
+     var aliveCells = 0;
+
+     for (var i = -1; i < 2; i++) {
+       for(var j = -1; j < 2; j++) {
+         if(i === 0 && i === j) {
+           continue;
+         }
+         var currentCell = this.getCellAt(x + i, y + j);
+
+         if(currentCell && currentCell.isAlive()) {
+           aliveCells++;
+         }
+       }
+     }
+     return aliveCells;
+   },
+   calculateNextState: function(cell) {
+     var tempCell = new Cell(cell.x, cell.y, cell.alive);
+     var livingNeighbours = this.getAliveNeighbours(cell);
+     if(livingNeighbours === 2 || livingNeighbours === 3) {
+       tempCell.alive = true;
+     } else {
+       tempCell.alive = false;
+     }
+     return tempCell;
+    }
 };
 
-Board.prototype = {
-  addCell: function(cell) {
-    this.cells[getCellRepresentation(cell.x,cell.y)] = cell;
-  },
-  getCellAt: function(x,y) {
-    return this.cells[getCellRepresentation(x,y)];
-  },
-  getAliveNeighbours: function(cell) {
-    return 0;
+var Cell = function (x, y, alive) {
+  this.x = x;
+  this.y = y;
+  this.alive = alive;
+};
+
+Cell.prototype = {
+  isAlive: function() {
+    return this.alive;
   }
 };
 
-function getCellRepresentation(x,y) {
-  return "x" + x + "y" + y;
+function getCellRepresentation (x, y){
+ return "x" + x + "y" + y;
 }
--- test/spec.js
+++ test/spec.js
@@ -25,9 +25,36 @@ describe('Conways game of life', function() {
 
   describe('getAliveNeighbours', function() {
     it('returns 0 when there are no alive neighbours', function() {
+      expect(board.getAliveNeighbours(cell)).toEqual(0);
+    });
+    it('returns 1 when there is 1 alive neighbour', function() {
       var neighbourCell = new Cell(1,2,true);
       board.addCell(neighbourCell);
-      expect(board.getAliveNeighbours(cell)).toEqual(0);
+      expect(board.getAliveNeighbours(cell)).toEqual(1);
+    });
+    it('returns 8 when there are 8 alive neighbours', function() {
+      board.addCell(new Cell(0,0,true));
+      board.addCell(new Cell(0,1,true));
+      board.addCell(new Cell(1,0,true));
+      board.addCell(new Cell(1,2,true));
+      board.addCell(new Cell(2,1,true));
+      board.addCell(new Cell(2,0,true));
+      board.addCell(new Cell(0,2,true));
+      board.addCell(new Cell(2,2,true));
+
+      expect(board.getAliveNeighbours(cell)).toEqual(8);
+    });
+    it('returns 1 when there are 7 dead neighbours', function() {
+      board.addCell(new Cell(0,0,true));
+      board.addCell(new Cell(0,1,false));
+      board.addCell(new Cell(1,0,false));
+      board.addCell(new Cell(1,2,false));
+      board.addCell(new Cell(2,1,false));
+      board.addCell(new Cell(2,0,false));
+      board.addCell(new Cell(0,2,false));
+      board.addCell(new Cell(2,2,false));
+
+    expect(board.getAliveNeighbours(cell)).toEqual(1);
     });
   });
 });
